13d4cbe8b596941832bf2eb15440bc03383a0495
Jinwoo Kim <jinwookim@Jinwooui-MacBookPro.local>
2022-08-09 01:33:02 +0900

    check load

---
+++ large-scale-regression/scripts/visualize-pcqv2-lap-performer-finetune.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ulimit -c unlimited
+
+python ../visualize/visualize_attn_dist.py \
+--user-dir ../tokengt \
+--num-workers 16 \
+--ddp-backend=legacy_ddp \
+--dataset-name pcqm4mv2 \
+--dataset-source ogb \
+--task graph_prediction \
+--criterion l1_loss \
+--arch tokengt_base_ablated \
+--prenorm \
+--num-classes 1 \
+--batch-size 64 \
+--data-buffer-size 20 \
+--save-dir ckpts/pcqv2-tokengt-ablated-pretrained \
+--split valid \
+--seed 12 \
+--return-attention
